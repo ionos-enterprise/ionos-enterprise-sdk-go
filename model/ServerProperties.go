9dea2c24a0dc0740d93fd621ce845a1faede6826
jasminSPC <jasmin@stackpointcloud.com>
2016-07-07 17:47:24 +0200

    .

--- model/ServerProperties.go
+++ model/ServerProperties.go
@@ -2,8 +2,8 @@ package model
 
 type ServerProperties struct {
 	Name             string  `json:"name,omitempty"`
-	Cores            int32  `json:"cores,omitempty"`
-	Ram              int32  `json:"ram,omitempty"`
+	Cores            int  `json:"cores,omitempty"`
+	Ram              int  `json:"ram,omitempty"`
 	AvailabilityZone string  `json:"availabilityZone,omitempty"`
 	VmState          string  `json:"vmState,omitempty"`
 }
