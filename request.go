6882da448620a393cf712a759480ba355e8e0e71
Christian Schulze-Wiehenbrauk <schulze-wiehenbrauk@strato-rz.de>
2019-07-11 18:16:27 +0200

    New methods for request waiting loops (#47)
    WaitTillRequestsFinished: Wait until ListRequests with filter returns
    only finished requests
    
    WaitTillMatchingRequestsFinished: Wait until ListRequests with filter
    returns only finished requests with additional selector to fine tune the
    filters.

--- request.go
+++ request.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strconv"
 	"time"
 )
@@ -71,12 +72,102 @@ type Request struct {
 	StatusCode int               `json:"statuscode,omitempty"`
 }
 
+// RequestListFilter is a wrapper around url.Values to provide a common
+// interface to make use of the filters that the ionos API provides for the
+// requests endpoint.
+// Example:
+//   filter := NewRequestListFilter().WithUrl("volumes").WithBody("de/fra") will create a api call
+//   with query args like: /requests?filter.url=volumes&filter.body=de%2Ffra
+type RequestListFilter struct {
+	url.Values
+}
+
+// NewRequestListFilter creates a new RequestListFilter
+func NewRequestListFilter() *RequestListFilter {
+	return &RequestListFilter{Values: url.Values{}}
+}
+
+// AddUrl adds an url filter to the request list filter
+func (f *RequestListFilter) AddUrl(url string) {
+	f.WithUrl(url)
+}
+
+// WithUrl adds an url filter to the request list filter returning the filter for chaining
+func (f *RequestListFilter) WithUrl(url string) *RequestListFilter {
+	f.Add("filter.url", url)
+	return f
+}
+
+// AddCreatedDate adds a createdDate filter to the request list filter
+func (f *RequestListFilter) AddCreatedDate(createdDate string) {
+	f.WithCreatedDate(createdDate)
+}
+
+// WithCreatedDate adds a createdDate filter to the request list filter returning the filter for chaining
+func (f *RequestListFilter) WithCreatedDate(createdDate string) *RequestListFilter {
+	f.Add("filter.createdDate", createdDate)
+	return f
+}
+
+// AddMethod adds a method filter to the request list filter
+func (f *RequestListFilter) AddMethod(method string) {
+	f.WithMethod(method)
+}
+
+// WithMethod adds a method filter to the request list filter returning the filter for chaining
+func (f *RequestListFilter) WithMethod(method string) *RequestListFilter {
+	f.Add("filter.method", method)
+	return f
+}
+
+// AddBody adds a body filter to the request list filter
+func (f *RequestListFilter) AddBody(body string) {
+	f.WithBody(body)
+}
+
+// WithBody adds a body filter to the request list filter returning the filter for chaining
+func (f *RequestListFilter) WithBody(body string) *RequestListFilter {
+	f.Add("filter.body", body)
+	return f
+}
+
+// AddRequestStatus adds a requestStatus filter to the request list filter
+func (f *RequestListFilter) AddRequestStatus(requestStatus string) {
+	f.WithRequestStatus(requestStatus)
+}
+
+// WithRequestStatus adds a requestStatus filter to the request list filter returning the filter for chaining
+func (f *RequestListFilter) WithRequestStatus(requestStatus string) *RequestListFilter {
+	f.Add("filter.requestStatus", requestStatus)
+	return f
+}
+
 // ListRequests lists all requests
 func (c *Client) ListRequests() (*Requests, error) {
 	url := "/requests" + `?depth=` + c.client.depth + `&pretty=` + strconv.FormatBool(c.client.pretty)
 	ret := &Requests{}
 	err := c.client.Get(url, ret, http.StatusOK)
 	return ret, err
+}
+
+// ListRequestsWithFilter lists all requests that match the given filters
+func (c *Client) ListRequestsWithFilter(filter *RequestListFilter) (*Requests, error) {
+	path := "/requests"
+	query := url.Values{
+		"depth": []string{"10"},
+	}
+	if filter != nil {
+		for k, v := range filter.Values {
+			for _, i := range v {
+				query.Add(k, i)
+			}
+		}
+	}
+	path += "?" + query.Encode()
+	ret := &Requests{}
+	err := c.client.Get(path, ret, http.StatusOK)
+	return ret, err
+
 }
 
 // GetRequest gets a specific request
@@ -87,7 +178,7 @@ func (c *Client) GetRequest(reqID string) (*Request, error) {
 	return ret, err
 }
 
-// GetRequestStatus retursn status of the request
+// GetRequestStatus returns status of the request
 func (c *Client) GetRequestStatus(path string) (*RequestStatus, error) {
 	url := path + `?depth=` + c.client.depth + `&pretty=` + strconv.FormatBool(c.client.pretty)
 	ret := &RequestStatus{}
@@ -95,44 +186,53 @@ func (c *Client) GetRequestStatus(path string) (*RequestStatus, error) {
 	return ret, err
 }
 
+// IsRequestFinished checks the given path to a request status resource. The request is considered "done"
+// if its status won't change, which is true for status FAILED and DONE. Since Failed is obviously not done,
+// the method returns true and RequestFailed error in that case.
+func (c *Client) IsRequestFinished(path string) (bool, error) {
+	request, err := c.GetRequestStatus(path)
+	if err != nil {
+		return false, err
+	}
+	switch request.Metadata.Status {
+	case "DONE":
+		return true, nil
+	case "FAILED":
+		return true, NewClientError(
+			RequestFailed,
+			fmt.Sprintf("Request %s failed: %s", request.ID, request.Metadata.Message),
+		)
+	}
+	return false, nil
+}
+
 // WaitTillProvisionedOrCanceled waits for a request to be completed.
 // It returns an error if the request status could not be fetched, the request
 // failed or the given context is canceled.
 func (c *Client) WaitTillProvisionedOrCanceled(ctx context.Context, path string) error {
+	done, err := c.IsRequestFinished(path)
+	if err != nil {
+		return err
+	} else if done {
+		return nil
+	}
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
-	for {
-		select {
-		case <-ctx.Done():
+	for !done {
+		if ctx.Err() != nil {
 			return ctx.Err()
-		default:
 		}
-
-		// The order of a select's cases does not imply a priority for Go.
-		// If the work done on every tick takes longer than the tick interval
-		// and the context is cancelled, both channels can be read.
-		// This is a race condition as the cancelled context might be ignored
-		// indefinitely. To mitigate this behavior, we non-blockingly recheck
-		// the context's cancellation at the beginning of every loop run.
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		case <-ticker.C:
-			request, err := c.GetRequestStatus(path)
+			done, err = c.IsRequestFinished(path)
 			if err != nil {
 				return err
 			}
-			switch request.Metadata.Status {
-			case "DONE":
-				return nil
-			case "FAILED":
-				return NewClientError(
-					RequestFailed,
-					fmt.Sprintf("Request %s failed: %s", request.ID, request.Metadata.Message),
-				)
-			}
 		}
 	}
+	return nil
 }
 
 // WaitTillProvisioned waits for a request to be completed.
@@ -147,4 +247,52 @@ func (c *Client) WaitTillProvisioned(path string) (err error) {
 		}
 	}
 	return
+}
+
+type RequestSelector func(Request) bool
+
+// IsRequestStatusFinished is true if the requests Status is neither QUEUED or RUNNING.
+func IsRequestStatusFinished(r Request) bool {
+	switch r.Metadata.RequestStatus.Metadata.Status {
+	case "QUEUED", "RUNNING":
+		return false
+	}
+	return true
+}
+
+// WaitTillRequestsFinished will wait until there are no more unfinished requests matching the given filter
+func (c *Client) WaitTillRequestsFinished(ctx context.Context, filter *RequestListFilter) error {
+	return c.WaitTillMatchingRequestsFinished(ctx, filter, func(r Request) bool { return !IsRequestStatusFinished(r) })
+}
+
+// WaitTillMatchingRequestsFinished gets open requests with given filters and will
+// wait for each request that is selected by the selector. The selector
+// should consider filtering out requests that are finished. (e.g. using IsRequestStatusFinished)
+func (c *Client) WaitTillMatchingRequestsFinished(
+	ctx context.Context, filter *RequestListFilter, selector RequestSelector) error {
+
+	waited := true
+	for waited && ctx.Err() == nil {
+
+		waited = false
+		requests, err := c.ListRequestsWithFilter(filter)
+		if err != nil {
+			return err
+		}
+		for _, r := range requests.Items {
+			if selector(r) {
+				waited = true
+				if err := c.WaitTillProvisionedOrCanceled(ctx, r.Metadata.RequestStatus.Href); err != nil {
+					if !IsRequestFailed(err) {
+						return err
+					}
+
+				}
+			}
+		}
+		if !waited {
+			break
+		}
+	}
+	return nil
 }
---
+++ request_test.go
@@ -0,0 +1,93 @@
+package profitbricks
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path/filepath"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+const requestTestDataPath = "testdata"
+
+type SuiteRequest struct {
+	suite.Suite
+	client *Client
+	apiUrl string
+}
+
+func (s *SuiteRequest) SetupTest() {
+	s.client = NewClient("", "")
+	s.apiUrl = s.client.client.apiURL
+	httpmock.Activate()
+}
+
+func (s *SuiteRequest) TearDownTest() {
+	httpmock.DeactivateAndReset()
+}
+
+type SuiteWaitTillRequests struct {
+	SuiteRequest
+}
+
+func Test_WaitTillRequests(t *testing.T) {
+	suite.Run(t, new(SuiteWaitTillRequests))
+}
+
+func loadTestData(t *testing.T, filename string) []byte {
+	data, err := ioutil.ReadFile(filepath.Join(requestTestDataPath, filename))
+	assert.NoError(t, err)
+	return data
+}
+
+func (s *SuiteWaitTillRequests) Test_OK_NoSelector() {
+	listResponses := [][]byte{
+		loadTestData(s.T(), "request_request_till_no_request_matches_01.json"),
+		loadTestData(s.T(), "request_request_till_no_request_matches_02.json"),
+	}
+	statusResponses := [][]byte{
+		loadTestData(s.T(), "request_queued.json"),
+		loadTestData(s.T(), "request_done.json"),
+	}
+	query := url.Values{
+		"filter.url": []string{"volumes"},
+		"depth":      []string{"10"},
+	}
+	httpmock.RegisterResponderWithQuery(http.MethodGet, s.apiUrl+"/requests", query,
+		httpmock.NewBytesResponder(200, listResponses[0]).Once())
+	httpmock.RegisterResponderWithQuery(http.MethodGet, s.apiUrl+"/requests", query,
+		httpmock.NewBytesResponder(200, listResponses[1]).Once())
+
+	httpmock.RegisterResponder(http.MethodGet, "=~/requests/.*/status.*",
+		httpmock.NewBytesResponder(200, statusResponses[0]).Once())
+	httpmock.RegisterResponder(http.MethodGet, "=~/requests/.*/status.*",
+		httpmock.NewBytesResponder(200, statusResponses[1]).Once())
+
+	err := s.client.WaitTillRequestsFinished(context.Background(), NewRequestListFilter().WithUrl("volumes"))
+	s.NoError(err)
+}
+
+func (s *SuiteWaitTillRequests) Test_Err_ListError() {
+	httpmock.RegisterResponder(http.MethodGet, s.apiUrl+"/requests",
+		httpmock.NewStringResponder(401, "{}"))
+	err := s.client.WaitTillMatchingRequestsFinished(context.Background(), nil, nil)
+	s.Error(err)
+	s.Equal(1, httpmock.GetTotalCallCount())
+}
+
+func (s *SuiteWaitTillRequests) Test_Err_GetStatusError() {
+	rsp := loadTestData(s.T(), "request_request_till_no_request_matches_01.json")
+	httpmock.RegisterResponder(http.MethodGet, s.apiUrl+"/requests",
+		httpmock.NewBytesResponder(200, rsp))
+
+	httpmock.RegisterResponder(http.MethodGet, "=~/requests/.*/status.*",
+		httpmock.NewStringResponder(401, "{}"))
+	err := s.client.WaitTillRequestsFinished(context.Background(), nil)
+	s.Error(err)
+	s.Equal(2, httpmock.GetTotalCallCount())
+}
